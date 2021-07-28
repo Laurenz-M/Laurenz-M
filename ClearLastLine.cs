4e4e9bab30ee58b90229f5d8d9c8aaab2b0d6688
Laurenz-M <88063622+Laurenz-M@users.noreply.github.com>
2021-07-28 19:20:27 +0200

    Add prototype of ClearLastNLines
    I plan on using this function to clear the last x number of lines inside a console so that I can quit using "Console.Clear( );" to clear the whole console.

---
+++ ClearLastLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine("Test");
+        Console.Write("             Test2");
+        Thread.Sleep(1000);
+        ClearLastNLines(2);
+        Console.ReadLine();
+    }
+
+    /*public static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLineCursor);
+    }*/
+
+    public static void ClearLastNLines(int NumberOfLines) 
+    {
+        for(int LineNumber = 0; LineNumber < NumberOfLines; LineNumber++)
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop - LineNumber);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+    }
+}
